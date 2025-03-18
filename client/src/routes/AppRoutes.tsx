062571e0893951e791201c4eec7a3acacce7144e
Linoyvak <Linoyvak@gmail.com>
2025-03-19 00:47:06 +0200

    Navbar design

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Home from "../pages/Home";
 
 
 export const AppRoutes = () => {
@@ -9,6 +10,12 @@ export const AppRoutes = () => {
       {/* Public Routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      <Route path="/" element={<Home />}>
+
+        {/* Nested routes will render inside Feed's Outlet */}
+      </Route>
+
 
 
 
